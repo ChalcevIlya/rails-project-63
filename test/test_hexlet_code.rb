25e1607d5bf73bd6bec0d950b5b50607d21e579d
Chalcev Ilya <mr.chalcev@bk.ru>
2025-04-17 21:21:51 +0300

    Исправления линтера

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,6 +2,7 @@
 
 require "test_helper"
 
+# Tests for form generator
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
@@ -9,10 +10,10 @@ class TestHexletCode < Minitest::Test
 
   def test_that_it_generate_form_tag
     user_class = Struct.new(:name, :job, keyword_init: true)
-    user = user_class.new name: 'rob'
-    
-    assert (::HexletCode.form_for user, url: '/profile', class: 'hexlet-form') == '<form action="/profile" method="post" class="hexlet-form"></form>'
+    user = user_class.new name: "rob"
+
+    assert (HexletCode.form_for user, class: "hexlet-form") == '<form action="#" method="post" class="hexlet-form"></form>'
     assert (HexletCode.form_for user) == '<form action="#" method="post"></form>'
-    assert (HexletCode.form_for user, url: '/profile', class: 'hexlet-form') == '<form action="/profile" method="post" class="hexlet-form"></form>'
+    assert (HexletCode.form_for user, url: "/profile", class: "hexlet-form") == '<form action="/profile" method="post" class="hexlet-form"></form>'
   end
 end
