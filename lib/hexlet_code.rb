6c4cb70d74fcd5a66f35bddcf4f78bfbc647349b
Chalcev Ilya <mr.chalcev@bk.ru>
2025-04-24 23:06:00 +0300

    Финальные исправления по линтеру

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
-autoload(:Tag, "tag.rb")
+require_relative 'hexlet_code/version'
+autoload(:Tag, 'tag.rb')
 # Main module
 module HexletCode
   class Error < StandardError; end
 
   def self.form_for(structure, **attributes)
-    @result = ""
-    attributes = { action: attributes.include?(:url) ? attributes.delete(:url) : "#", method: "post" }.merge!(attributes)
-    @result += Tag.build("form", **attributes)
+    @result = ''
+    attributes = { action: attributes.include?(:url) ? attributes.delete(:url) : '#', method: 'post' }.merge!(attributes)
+    @result += Tag.build('form', **attributes)
     if block_given?
       @structure = structure
       yield(self)
@@ -22,18 +22,18 @@ module HexletCode
     field_name = field.to_s
     temp_hash[:name] = field_name
     value = @structure.public_send(field)
-    @result += Tag.build("label", for: field_name) { field_name.capitalize }
+    @result += Tag.build('label', for: field_name) { field_name.capitalize }
     case attributes.delete(:as)
     when :text
-      temp_hash.merge!({ cols: attributes.include?(:cols) ? attributes.delete(:cols) : "20",
-                         rows: attributes.include?(:rows) ? attributes.delete(:rows) : "40" })
-      @result += Tag.build("textarea", **temp_hash, **attributes) { value }
+      temp_hash.merge!({ cols: attributes.include?(:cols) ? attributes.delete(:cols) : '20',
+                         rows: attributes.include?(:rows) ? attributes.delete(:rows) : '40' })
+      @result += Tag.build('textarea', **temp_hash, **attributes) { value }
     else
-      @result += Tag.build("input", **temp_hash, type: "text", value: value, **attributes)
+      @result += Tag.build('input', **temp_hash, type: 'text', value: value, **attributes)
     end
   end
 
-  def self.submit(button_text = "Save")
-    @result += Tag.build("input", type: "submit", value: button_text)
+  def self.submit(button_text = 'Save')
+    @result += Tag.build('input', type: 'submit', value: button_text)
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 # Tests for form generator
 class TestHexletCode < Minitest::Test
@@ -10,18 +10,18 @@ class TestHexletCode < Minitest::Test
 
   def test_that_it_generate_form_tag
     user_class = Struct.new(:name, :job, keyword_init: true)
-    user = user_class.new name: "rob"
+    user = user_class.new name: 'rob'
 
-    assert (HexletCode.form_for user, class: "hexlet-form") == '<form action="#" method="post" class="hexlet-form"></form>'
+    assert (HexletCode.form_for user, class: 'hexlet-form') == '<form action="#" method="post" class="hexlet-form"></form>'
     assert (HexletCode.form_for user) == '<form action="#" method="post"></form>'
-    assert (HexletCode.form_for user, url: "/profile", class: "hexlet-form") == '<form action="/profile" method="post" class="hexlet-form"></form>'
+    assert (HexletCode.form_for user, url: '/profile', class: 'hexlet-form') == '<form action="/profile" method="post" class="hexlet-form"></form>'
   end
 
   def test_generating_full_form_without_additional_attrs
     user_class = Struct.new(:name, :job, :gender, keyword_init: true)
-    user = user_class.new name: "rob", job: "hexlet", gender: "m"
+    user = user_class.new name: 'rob', job: 'hexlet', gender: 'm'
 
-    file = File.open("test/from_tag_without_additional_attrs.html")
+    file = File.open('test/from_tag_without_additional_attrs.html')
     assert (HexletCode.form_for user do |f|
       f.input :name
       f.input :job, as: :text
@@ -31,11 +31,11 @@ class TestHexletCode < Minitest::Test
 
   def test_generating_full_form_with_attrs
     user_class = Struct.new(:name, :job, :gender, keyword_init: true)
-    user = user_class.new name: "rob", job: "hexlet", gender: "m"
+    user = user_class.new name: 'rob', job: 'hexlet', gender: 'm'
 
-    file = File.open("test/form_tag_with_attrs.html")
-    assert (HexletCode.form_for user, url: "#" do |f|
-      f.input :name, class: "user-input"
+    file = File.open('test/form_tag_with_attrs.html')
+    assert (HexletCode.form_for user, url: '#' do |f|
+      f.input :name, class: 'user-input'
       f.input :job
     end) == file.read
     file.close
@@ -43,10 +43,10 @@ class TestHexletCode < Minitest::Test
 
   def test_generating_full_form_with_overriding_attrs
     user_class = Struct.new(:name, :job, :gender, keyword_init: true)
-    user = user_class.new name: "rob", job: "hexlet", gender: "m"
+    user = user_class.new name: 'rob', job: 'hexlet', gender: 'm'
 
-    file = File.open("test/form_tag_with_overriding_attrs.html")
-    assert (HexletCode.form_for user, url: "#" do |f|
+    file = File.open('test/form_tag_with_overriding_attrs.html')
+    assert (HexletCode.form_for user, url: '#' do |f|
       f.input :job, as: :text, rows: 50, cols: 50
     end) == file.read
     file.close
@@ -54,9 +54,9 @@ class TestHexletCode < Minitest::Test
 
   def test_submit
     user_class = Struct.new(:name, :job, keyword_init: true)
-    user = user_class.new job: "hexlet"
+    user = user_class.new job: 'hexlet'
 
-    file = File.open("test/form_with_submit.html")
+    file = File.open('test/form_with_submit.html')
     assert (HexletCode.form_for user do |f|
       f.input :name
       f.input :job
@@ -67,13 +67,13 @@ class TestHexletCode < Minitest::Test
 
   def test_submit_override
     user_class = Struct.new(:name, :job, keyword_init: true)
-    user = user_class.new job: "hexlet"
+    user = user_class.new job: 'hexlet'
 
-    file = File.open("test/form_with_submit_override.html")
+    file = File.open('test/form_with_submit_override.html')
     assert (HexletCode.form_for user do |f|
       f.input :name
       f.input :job
-      f.submit "Wow"
+      f.submit 'Wow'
     end) == file.read
     file.close
   end
